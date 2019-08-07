ab6d862de2cfe152133a3fe6bfcc0b7c5776afdd
Katiuska Alicea <katiuskaalicea@gmail.com>
2019-08-06 19:54:05 -0700

    Created a test .js file

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+alert('are we linked?'); We are!!
+
+/*const myHeading = document.getElementById('myHeading');
+
+	myHeading.addEventListener('click', () => {
+		myHeading.style.color = 'red';
+	});*/
 
