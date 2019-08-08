4871793e1170adb09947e524d6acf92c28a6fc81
Katiuska Alicea <katiuskaalicea@gmail.com>
2019-08-07 20:18:20 -0700

    Cleared it and left only the link test, commented out

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-alert('are we linked?'); We are!!
+/*alert('are we linked?'); We are!!*/
 
-/*const myHeading = document.getElementById('myHeading');
 
-	myHeading.addEventListener('click', () => {
-		myHeading.style.color = 'red';
-	});*/
+
+
+	
 
